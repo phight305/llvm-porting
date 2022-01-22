b3097dc394438f3f0af49535db44af46c53dff18
phight305 <j199180305@gmail.com>
2022-01-22 22:29:22 +0800

    Peephole optimization for MOVi 0

--- lib/Target/TOY/TOYPreRegAlloc.cpp
+++ lib/Target/TOY/TOYPreRegAlloc.cpp
@@ -9,7 +9,10 @@
 
 #define DEBUG_TYPE "pre-reg-alloc"
 #include "TOY.h"
+#include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
+#include "llvm/Target/TargetRegisterInfo.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
 
@@ -23,6 +26,7 @@ namespace {
 
     TOYPreRegAlloc() : MachineFunctionPass(ID) {}
 
+    bool peepholeOpt(MachineFunction &MF);
     virtual bool runOnMachineFunction(MachineFunction &MF);
 
     const char *getPassName() const { return "TOY PreRegAlloc"; }
@@ -43,8 +47,49 @@ FunctionPass *llvm::createTOYPreRegAlloc() {
   return new TOYPreRegAlloc();
 }
 
+bool TOYPreRegAlloc::peepholeOpt(MachineFunction &MF) {
+  bool Changed = false;
+
+  MachineRegisterInfo *MRI = &MF.getRegInfo();
+
+  for (MachineFunction::iterator MFI = MF.begin(), MFE = MF.end(); MFI != MFE; MFI++) {
+    for (MachineBasicBlock::iterator MBBI = MFI->begin(), MBBE = MFI->end(); MBBI != MBBE;) {
+      MachineInstr *MI = &*MBBI;
+      MBBI++;
+      unsigned int Opcode = MI->getOpcode();
+
+      switch (Opcode) {
+      case TOY::MOVi: {
+          int64_t Val = MI->getOperand(1).getImm();
+          if (Val == 0) {
+            unsigned int Reg = MI->getOperand(0).getReg();
+            if (TargetRegisterInfo::isVirtualRegister(Reg)) {
+              for (MachineRegisterInfo::use_nodbg_iterator UI = MRI->use_nodbg_begin(Reg), UE = MRI->use_nodbg_end(); UI != UE;) {
+                MachineOperand &MO = UI.getOperand();
+                ++UI;
+                MO.setReg(TOY::ZERO);
+                Changed = true;
+              }
+
+              if (MRI->use_nodbg_empty(Reg))
+                  MI->eraseFromParent();
+            }
+          }
+        }
+        break;
+      default: break;
+      }
+    }
+  }
+  return Changed;
+}
+
 bool TOYPreRegAlloc::runOnMachineFunction(MachineFunction &MF) {
   DEBUG(dbgs() << "********* TOY PreRegAlloc *********\n");
 
-  return false;
+  bool Changed = false;
+
+  Changed |= peepholeOpt(MF);
+
+  return Changed;
 }
