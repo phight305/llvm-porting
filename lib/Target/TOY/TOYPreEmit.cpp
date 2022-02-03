f3129865aa9b6d0b3f069500917bc88f3fdc2bec
phight305 <j199180305@gmail.com>
2022-02-03 10:34:03 +0800

    Add local value numbering in TOYPreEmit

--- lib/Target/TOY/TOYPreEmit.cpp
+++ lib/Target/TOY/TOYPreEmit.cpp
@@ -33,6 +33,7 @@ namespace {
     virtual bool runOnMachineFunction(MachineFunction &MF);
     bool optimizeLoopBranch(MachineFunction &MF);
     bool removeFallThroughBranch(MachineFunction &MF);
+    bool localValueNumbering(MachineFunction &MF);
 
     const char *getPassName() const { return "TOY PreRmit"; }
 
@@ -150,6 +151,108 @@ bool TOYPreEmit::removeFallThroughBranch(MachineFunction &MF) {
   return Changed;
 }
 
+namespace {
+
+struct Val {
+  unsigned int Op;
+  std::vector<unsigned int> Nums;
+  Val(unsigned int Op, std::vector<unsigned int> Nums) : Op(Op), Nums(Nums) {}
+};
+
+}
+
+static bool isLvnCandidate(MachineInstr *MI) {
+  switch (MI->getOpcode()) {
+  case TOY::ADDri:
+  case TOY::ADDrr:
+  case TOY::SUBri:
+  case TOY::MOVi: return true;
+  default: return false;
+  }
+}
+
+bool TOYPreEmit::localValueNumbering(MachineFunction &MF) {
+
+  bool Changed = false;
+
+  for (MachineFunction::iterator MFI = MF.begin(); MFI != MF.end(); MFI++) {
+    unsigned int Num = 0;
+    std::map<unsigned int, unsigned int> Reg2NumMap;
+    std::map<int64_t, unsigned int> Imm2NumMap;
+    std::map<unsigned int, Val *> Num2ValMap;
+    for (MachineBasicBlock::iterator MBBI = MFI->begin(), MBBE = MFI->end(); MBBI != MBBE;) {
+      MachineInstr *MI = &*MBBI;
+      MBBI++;
+
+      if (MI->getOpcode() == TOY::BR || MI->getOpcode() == TOY::BRCC)
+        continue;
+
+      std::vector<unsigned int> Nums;
+      unsigned int DefReg = 0;
+      for (unsigned int i = 0; i < MI->getNumOperands(); i++) {
+        MachineOperand &Operand = MI->getOperand(i);
+        if (Operand.isReg()) {
+          unsigned int Reg = Operand.getReg();
+          if (Operand.isDef()) {
+            if (DefReg != 0)
+              llvm_unreachable("There should be only one defined reg!");
+            DefReg = Reg;
+            continue;
+          }
+
+          if (Reg2NumMap.find(Reg) != Reg2NumMap.end()) {
+            Nums.push_back(Reg2NumMap[Reg]);
+          } else {
+            Reg2NumMap[Reg] = Num;
+            Num2ValMap[Num] = NULL;
+            Nums.push_back(Num);
+            Num++;
+          }
+        } else if (Operand.isImm()) {
+          int64_t Imm = Operand.getImm();
+          if (Imm2NumMap.find(Imm) != Imm2NumMap.end()) {
+            Nums.push_back(Imm2NumMap[Imm]);
+          } else {
+            Imm2NumMap[Imm] = Num;
+            Num2ValMap[Num] = NULL;
+            Nums.push_back(Num);
+            Num++;
+          }
+        } else {
+          llvm_unreachable("The MO should be reg or imm!");
+        }
+      }
+
+      if (DefReg != 0) {
+        if (Reg2NumMap.find(DefReg) != Reg2NumMap.end()) {
+          unsigned int Num = Reg2NumMap[DefReg];
+          Val *Val = Num2ValMap[Num];
+          if (Val != NULL &&
+              Val->Nums.size() == Nums.size() &&
+              std::equal(Val->Nums.begin(), Val->Nums.end(), Nums.begin())) {
+            if (isLvnCandidate(MI)) {
+              MI->eraseFromParent();
+              Changed = true;
+              continue;
+            }
+          }
+        }
+
+        Reg2NumMap[DefReg] = Num;
+        Num2ValMap[Num] = new Val(MI->getOpcode(), Nums);
+        Num++;
+      }
+    }
+
+    std::map<unsigned int, Val *>::iterator Num2ValIter;
+    for (Num2ValIter = Num2ValMap.begin(); Num2ValIter != Num2ValMap.end(); Num2ValIter++) {
+      delete Num2ValIter->second;
+    }
+  }
+
+  return Changed;
+}
+
 bool TOYPreEmit::runOnMachineFunction(MachineFunction &MF) {
   DEBUG(dbgs() << "********* TOY PreRmit *********\n");
 
@@ -162,6 +265,7 @@ bool TOYPreEmit::runOnMachineFunction(MachineFunction &MF) {
      branching to its fall through BB.
   */
   Changed |= removeFallThroughBranch(MF);
+  Changed |= localValueNumbering(MF);
 
   return Changed;
 }
